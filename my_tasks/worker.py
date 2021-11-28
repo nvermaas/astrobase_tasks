c3fdaf28fe1f0874174b016023451d500dc96398
Nico Vermaas <vermaas@astron.nl>
2021-11-28 12:07:01 +0100

    use eventlet instead of gevent

--- my_tasks/worker.py
+++ my_tasks/worker.py
@@ -1,6 +1,9 @@
+import os
 from celery import Celery
 
 RABBITMQ_BROKER = "amqp://celery:celery@192.168.178.37:5672/celery"
+#RABBITMQ_BROKER = os.environ['RABBITMQ_BROKER']
+
 app = Celery('tasks',
              backend='rpc://',
              broker=RABBITMQ_BROKER,
