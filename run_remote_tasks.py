e500c4221b5527ac5ba82ee773e40acdd8e5efda
Nico Vermaas <vermaas@astron.nl>
2021-11-28 19:57:39 +0100

    add dir task

--- run_remote_tasks.py
+++ run_remote_tasks.py
@@ -5,10 +5,11 @@ def run_ping():
     print(task.get())  # pong my remote app
 
 def run_dir(path):
-    task = app.send_task("my_tasks.dir", kwargs=dict(path=path))
+    task = app.send_task("my_tasks.dir", kwargs=dict(my_path=path))
     return(task.get())  # pong my remote app
 
 # client program to test access to celery/broker
 if __name__ == '__main__':
     run_ping()
     print(run_dir("/shared"))
+    print(run_dir("/data"))
