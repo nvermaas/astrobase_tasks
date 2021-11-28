c30dc1c53a05da904a2fe3a84de317f06527be98
Nico Vermaas <vermaas@astron.nl>
2021-11-28 16:09:11 +0100

    simplifyling tasks

---
+++ my_tasks.py
@@ -0,0 +1,28 @@
+import os
+from celery import Celery
+
+RABBITMQ_BROKER = "amqp://celery:celery@192.168.178.37:5672/celery"
+#RABBITMQ_BROKER = os.environ['RABBITMQ_BROKER']
+
+app = Celery('my_tasks',
+             backend='rpc://',
+             broker=RABBITMQ_BROKER)
+
+# Optional configuration, see the application user guide.
+app.conf.update(
+    result_expires=3600,
+)
+
+@app.task
+def add(x, y):
+    for i in range(1000):
+        print(i)
+    return x + y + i
+
+@app.task
+def ping(name):
+    # demo task to see if this can be reached from Django
+    return "pong " + name
+
+
+
