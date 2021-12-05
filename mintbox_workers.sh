a42f5bead8ac4dcc1dd0a35c4f0a88f17822c75f
vermaas <vermaas@astron.nl>
2021-12-05 19:20:43 +0100

    running worker on mintbox

---
+++ mintbox_workers.sh
@@ -0,0 +1,8 @@
+export RABBITMQ_BROKER=amqp://nvermaas:RaBbIt_2019@192.168.178.37:5672
+export ASTROBASE_URL=http://192.168.178.37:8008/my_astrobase/
+export ASTROBASE_USER=nvermaas
+export ASTROBASE_PASSWORD=StErReN_2020
+export QUEUE_ASTRO=astro
+export POLLING_IN_SECONDS=120
+export LOCAL_DATA_DIR=/data
+celery -A astro_tasks.tasks worker --pool=eventlet -l info --queues astro
