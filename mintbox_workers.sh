45893f2e7787d09a1428f1668aa916bfbdcfad6c
Nico Vermaas <vermaas@astron.nl>
2021-12-06 19:11:14 +0100

    try without docker

--- mintbox_workers.sh
+++ mintbox_workers.sh
@@ -3,6 +3,6 @@ export ASTROBASE_URL=http://192.168.178.37:8008/my_astrobase/
 export ASTROBASE_USER=nvermaas
 export ASTROBASE_PASSWORD=StErReN_2020
 export QUEUE_ASTRO=astro
-export POLLING_IN_SECONDS=120
+export POLLING_IN_SECONDS=60
 export LOCAL_DATA_DIR=/home/nvermaas/www/data_on_yggdrasil/astrobase/data
 celery -A astro_tasks.tasks worker --pool=eventlet -l info --queues astro
