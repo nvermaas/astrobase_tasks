478fd4d3646ee1456808a12f219234be1b90e1af
Vermaas <vermaas@astron.nl>
2023-07-16 12:17:12 +0200

    update documentation

--- astrobase_io.py
+++ astrobase_io.py
@@ -20,7 +20,6 @@ from astrobase_interface import AstroBase
 ASTROBASE_HEADER = {
     'content-type': "application/json",
     'cache-control': "no-cache",
-    #'authorization': "Basic YWRtaW46YWRtaW4="
 }
 
 
@@ -57,161 +56,6 @@ class AstroBaseIO:
         print(str(timestamp)+ ' - '+info_str)
 
 
-    def get_dir_size(self, start_path='.'):
-        total_size = 0
-        for dirpath, dirnames, filenames in os.walk(start_path, followlinks=True):
-            for f in filenames:
-                fp = os.path.join(dirpath, f)
-                total_size += os.path.getsize(fp)
-        return total_size
-
-# --- remote ssh commands---------------------------------------------------------------------------------------
-
-    # as found in copy_to_alta.py
-    def run_on_node(self, hostname, command, background=True):
-        """Run command on an ARTS node. Assumes ssh keys have been set up
-            node: nr of node (string or int)
-            command: command to run
-            background: whether to run ssh in the banode: nr of node (string or int)ckground
-        """
-        if background:
-            ssh_cmd = "ssh {} \"{}\" &".format(hostname, command)
-        else:
-            ssh_cmd = "ssh {} \"{}\"".format(hostname, command)
-        # print("Executing '{}'".format(ssh_cmd))
-        return os.system(ssh_cmd)
-
-
-    # copy a dataproduct from its hardcoded name (like tabA.fits) to the name provided by the atdb specfication service
-    def copy_dataproduct_remote(self, node, data_location, from_name, to_name):
-        """ copy a dataproduct from its hardcoded name (like tabA.fits) to the name provided by the atdb specfication service
-            node: nr of node (string or int)
-            data_location: directory on the node where the source file is, and where the target file will be copied.
-            from_name: file to copy
-            to_name : the new file.
-        """
-        #print('copy dataproduct ' + node + ':/' + data_location + '/' + from_name + ' to ' + to_name)
-        src = data_location + '/' + from_name
-        tgt = data_location + '/' + to_name
-        cmd = 'cp ' + src + ' ' + tgt
-
-        self.run_on_node(node, cmd, background=False)
-
-
-    # move/rename a remote dataproduct
-    def move_dataproduct_remote(self, node, data_location, from_name, to_name):
-        """ move/rename a remote dataproduct
-            node: nr of node (string or int)
-            data_location: directory on the node where the source file is, and where the target file will be copied.
-            from_name: filename to rename
-            to_name : new filename
-        """
-        #print('copy dataproduct ' + node + ':/' + data_location + '/' + from_name + ' to ' + to_name)
-        src = data_location + '/' + from_name
-        tgt = data_location + '/' + to_name
-        cmd = 'mv ' + src + ' ' + tgt
-
-        self.run_on_node(node, cmd, background=False)
-
-
-    # scp a dataproduct from a node to a local dir
-    def scp_dataproduct(self, node, from_location, from_name, to_location, to_name):
-        """ secure copy a file from a node to the current machine
-            node: nr of node (string or int)
-            data_location: directory on the node where the source file is, and where the target file will be copied.
-            from_name: file to copy
-            to_name : the new file.
-        """
-        #print('copy dataproduct ' + node + ':/' + data_location + '/' + from_name + ' to ' + to_name)
-        src = from_location + '/' + from_name
-        tgt = to_location + '/' + to_name
-        cmd = 'scp ' + node + ':' + src + ' ' + tgt
-        os.system(cmd)
-
-
-    # check if the specified file exist on the node.
-    def check_dataproduct(self, node, data_location, filename):
-        """ check if the dataproduct exists on the given location on the node)
-            node: nr of node (string or int)
-            data_location: directory on the node where the source file is, and where the target file will be copied.
-            from_name: file to check
-        """
-        src = data_location + '/' + filename
-        cmd = 'test -s ' + src
-        if self.run_on_node(node, cmd, background=False) == 0:
-            return True
-        else:
-            return False
-
-
-    # returns filesize of a remote file
-    def get_filesize_remote(self, node, filepath):
-        cmd = 'ssh ' + node + ' stat -Lc%s ' + filepath
-        try:
-            size = subprocess.check_output(cmd, shell=True)
-            size = size.split()[0].decode('utf-8')
-            return size
-        except Exception as err:
-            # file does not exist?
-            self.report('ERROR get_filesize_remote of' +filepath)
-            return 0
-
-
-    def remove_dataproduct_remote(self, node, data_location, filename):
-        """ remove a dataproduct from the given location on the node)
-            node: nr of node (string or int)
-            data_location: directory on the node where the source file is, and where the target file will be copied.
-            filename: file to remove
-        """
-        src = data_location + '/' + filename
-        cmd = 'rm ' + src
-        self.run_on_node(node, cmd, background=False)
-
-    # --------------------------------------------------------------------------------------------------------
-    # example: change status of observation with taskid=180223003 to valid.
-    # change_status('observations','taskid:180223003','valid'
-    def do_change_status(self, resource, search_key, status):
-
-        my_key = resource + ':new_status'   # observations:new_status
-
-        # id:27 or taskid:180223003
-        params = search_key.split(':')
-        search_field = params[0]            # taskid
-        search_value = params[1]            # 180223003
-
-        if search_field=='taskid':
-            if my_key.startswith('observations'):
-                self.astrobase_interface.do_PUT(key=my_key, id=None, taskid=search_value, value=status)
-            if  my_key.startswith('dataproducts'):
-                self.astrobase_interface.do_PUT_LIST(key=my_key, taskid=search_value, value=status)
-        else:
-            self.astrobase_interface.do_PUT(key=my_key, id=search_value, taskid=None, value=status)
-
-# --------------------------------------------------------------------------------------------------------
-    def do_delete_taskid(self, taskid):
-
-        # find the observation
-        cnt = 0
-        id = self.astrobase_interface.do_GET_ID(key='observations:taskID', value=taskid)
-        while (int(id) > 0):
-            cnt += 1
-            self.do_print('delete observation : ' + str(taskid) + ' (id = ' + str(id) + ')')
-            self.astrobase_interface.do_DELETE(resource='observations', id=id)
-            # check for more
-            id = self.astrobase_interface.do_GET_ID(key='observations:taskID', value=taskid)
-        print(str(cnt) + ' observations with taskID ' + str(taskid) + ' removed')
-
-        cnt = 0
-        id = self.astrobase_interface.do_GET_ID(key='dataproducts:taskID', value=taskid)
-        while (int(id) > 0):
-            cnt += 1
-            self.do_print('delete dataproduct for taskID ' + str(taskid) + ' (id = ' + str(id) + ')')
-            self.astrobase_interface.do_DELETE(resource='dataproducts', id=id)
-            # check for more
-            id = self.astrobase_interface.do_GET_ID(key='dataproducts:taskID', value=taskid)
-        print(str(cnt) + ' dataproducts with taskID ' + str(taskid) + ' removed')
-
-
 # --------------------------------------------------------------------------------------------------------
     def report(self, message, method='logging'):
         #self.do_print(message)
@@ -238,8 +82,7 @@ class AstroBaseIO:
             timestamp = datetime.datetime.now().strftime(self.TIME_FORMAT)
             payload = {"text": str(timestamp) + ' - ' + message_str}
 
-            # rabbits.do_send_message("amqp://nvermaas:<my_password>@192.168.178.37:5672/","slack",json.dumps(message_str))
-            # url = "https://hooks.slack.com/services/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" (fucked up by making it public)
+            # this no longer works, because the repo became public... never mind for now.
             url = "https://hooks.slack.com/services/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"
             res = requests.post(url, data=str(payload))
         except Exception as err:
